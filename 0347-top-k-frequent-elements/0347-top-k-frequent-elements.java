1c056219457a79b6376bf3a655b2b4103748ea2e
simrank0812 <simrankhatun816@gmail.com>
2026-07-31 23:52:05 +0530

    Time: 14 ms (70.85%), Space: 47.9 MB (12.95%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = pq.poll()[0];
+        }
+
+        return ans;
+    }
+}
