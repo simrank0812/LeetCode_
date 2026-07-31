62f31e49dca9d44128b95e5b94e99cee568f64d9
simrank0812 <simrankhatun816@gmail.com>
2026-07-31 23:53:01 +0530

    Time: 34 ms (26.61%), Space: 152.3 MB (37.99%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            // Remove indices out of the current window
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // Remove smaller elements from the back
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+
+            // Record the maximum for the current window
+            if (i >= k - 1) {
+                ans[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
