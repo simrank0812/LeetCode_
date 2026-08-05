de1feb46130cdf125b3cc2db9240cbb0bf9fafdf
simrank0812 <simrankhatun816@gmail.com>
2026-08-05 21:48:24 +0530

    Time: 71 ms (58.65%), Space: 282 MB (70.19%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            graph[i] = new ArrayList<>();
+
+        for (int[] e : invocations)
+            graph[e[0]].add(e[1]);
+
+        boolean[] suspicious = new boolean[n];
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(k);
+        suspicious[k] = true;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+
+            for (int v : graph[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+
+        // Check if any outside node calls a suspicious node
+        for (int[] e : invocations) {
+            int u = e[0];
+            int v = e[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++)
+                    ans.add(i);
+                return ans;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.add(i);
+        }
+
+        return ans;
+    }
+}
