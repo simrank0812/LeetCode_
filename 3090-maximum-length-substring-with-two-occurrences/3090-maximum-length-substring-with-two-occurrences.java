88de7888149ae345ccceb32f1c066770b77c232d
simrank0812 <simrankhatun816@gmail.com>
2026-08-17 19:25:08 +0530

    Time: 1 ms (100%), Space: 43.4 MB (84.48%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+
+        int[] freq = new int[26];
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+            freq[ch - 'a']++;
+
+            // If any character occurs more than twice,
+            // shrink the window
+            while (freq[ch - 'a'] > 2) {
+                freq[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
